7ae39e5ffb2e44400b212234f37387312da1180e
rinemafilm <rinamafilm@gmail.com>
2023-08-01 17:29:18 +0900

    0801

--- js/common.js
+++ js/common.js
@@ -2,5 +2,5 @@ $(function(){
     $('.head').load('../inc/header.html')
     $('.headMenu').load('../inc/headermenu.html')
     $('.headMenuVer2').load('../inc/headermenuVer2.html')
-    $('.footer').load('../inc/footer.html')
+    $('.Footer').load('../inc/footer.html')
 })
