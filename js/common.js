dc6e6c57f5db698dce6bb4e1bd990de3a4b1b536
rinemafilm <rinamafilm@gmail.com>
2023-08-01 13:46:06 +0900

    cardlist

--- js/common.js
+++ js/common.js
@@ -1,5 +1,6 @@
 $(function(){
     $('.head').load('../inc/header.html')
     $('.headMenu').load('../inc/headermenu.html')
+    $('.headMenuVer2').load('../inc/headermenuVer2.html')
     $('.footer').load('../inc/footer.html')
 })
